9692d2de37021c2f9c30a7e13320a0d1286aeb05
MonsterGamerz <100782480+MonsterGamerz@users.noreply.github.com>
2025-07-03 18:42:21 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: "class",
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
   theme: {
     extend: {},
   },
