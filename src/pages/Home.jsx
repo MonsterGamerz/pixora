aea30e32c9990ede4f9e0ccde252dc62edb08ff6
MonsterGamerz <100782480+MonsterGamerz@users.noreply.github.com>
2025-07-03 18:58:52 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+// src/pages/Home.jsx
+import React, { useEffect, useState } from 'react'
+import { db } from '../firebase'
+import { collection, getDocs } from 'firebase/firestore'
+import PostCard from '../components/PostCard'
+import StoryBar from '../components/StoryBar'
 
-const Home = () => {
-  return <div className="p-4 text-xl">🏠 Welcome to Pixora Home</div>
+export default function Home() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const snap = await getDocs(collection(db, 'posts'))
+      const data = snap.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+      setPosts(data)
+    }
+    fetchPosts()
+  }, [])
+
+  return (
+    <div className="max-w-xl mx-auto py-4 px-2">
+      <StoryBar />
+      <div className="space-y-4 mt-4">
+        {posts.map(post => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
+    </div>
+  )
 }
-
-export default Home
