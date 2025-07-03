a7cd4acd551dfeca3e1148a88db7d101610d92de
MonsterGamerz <100782480+MonsterGamerz@users.noreply.github.com>
2025-07-03 19:13:18 +0500

    Create ThemeContext.jsx

---
+++ src/ThemeContext.jsx
@@ -0,0 +1,34 @@
+import React, { createContext, useContext, useEffect, useState } from 'react'
+
+const ThemeContext = createContext()
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light')
+
+  useEffect(() => {
+    const localTheme = localStorage.getItem('theme')
+    if (localTheme) {
+      setTheme(localTheme)
+      document.documentElement.classList.add(localTheme)
+    } else {
+      document.documentElement.classList.add('light')
+    }
+  }, [])
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light'
+    setTheme(newTheme)
+    localStorage.setItem('theme', newTheme)
+
+    document.documentElement.classList.remove(theme)
+    document.documentElement.classList.add(newTheme)
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export const useTheme = () => useContext(ThemeContext)
