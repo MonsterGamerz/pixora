343274025ca89fe90581b2424f263825adab0043
MonsterGamerz <100782480+MonsterGamerz@users.noreply.github.com>
2025-07-04 20:04:52 +0500

    Update Assistant.jsx

--- src/pages/Assistant.jsx
+++ src/pages/Assistant.jsx
@@ -1,6 +1,6 @@
 // src/pages/Assistant.jsx
 import React, { useState } from 'react'
-import axios from 'axios'
+import axios from "axios";
 
 export default function Assistant() {
   const [input, setInput] = useState('')
