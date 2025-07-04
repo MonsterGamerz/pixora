d9d03f3cd16e0c347cf9d548cf2ceec55842fe65
MonsterGamerz <100782480+MonsterGamerz@users.noreply.github.com>
2025-07-04 21:16:53 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Reels from './pages/Reels';
+import Upload from './pages/Upload';
+import Chat from './pages/Chat';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
-import Upload from './pages/Upload';
 import Profile from './pages/Profile';
-import Reels from './pages/Reels';
-import Chat from './pages/Chat';
-import Navbar from './components/Navbar';
 import BottomNav from './components/BottomNav';
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/upload" element={<Upload />} />
-        <Route path="/profile/:uid" element={<Profile />} />
-        <Route path="/reels" element={<Reels />} />
-        <Route path="/chat" element={<Chat />} />
-      </Routes>
+      <div className="pb-16">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/reels" element={<Reels />} />
+          <Route path="/upload" element={<Upload />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile/:uid" element={<Profile />} />
+        </Routes>
+      </div>
       <BottomNav />
     </Router>
   );
