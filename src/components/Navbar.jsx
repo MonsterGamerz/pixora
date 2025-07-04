93620aff83529967898e8bd2e6c06a23f3155424
MonsterGamerz <100782480+MonsterGamerz@users.noreply.github.com>
2025-07-04 19:51:30 +0500

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="bg-gray-900 text-white p-4">
+      <h1 className="text-xl font-bold">Pixora</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
