eb9d8f941cd64a52306261dfad78776f9a9912f8
AXAAD <100782480+MonsterGamerz@users.noreply.github.com>
2025-07-09 14:46:55 +0500

    Update and rename PrivateRoute.jsx to ProtectsdRoute.jsx

--- src/components/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
-import { auth } from '../firebase'
-
-export default function PrivateRoute({ children }) {
-  const user = auth.currentUser
-  return user ? children : <Navigate to="/login" />
-}
