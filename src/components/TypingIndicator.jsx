fda16ce8ae14e28cc1af8ed7e8cec644ff4e6afa
AXAAD <100782480+MonsterGamerz@users.noreply.github.com>
2025-07-19 18:07:36 +0500

    Create TypingIndicator.jsx

---
+++ src/components/TypingIndicator.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TypingIndicator = () => {
+  return (
+    <div className="text-sm italic text-gray-500 mb-2 px-2">User is typing...</div>
+  );
+};
+
+export default TypingIndicator;
